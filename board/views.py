d688ace92eff1828326ac05633a501fc6ae70b71
hgo641 <hgo641@gmail.com>
2021-05-24 23:56:49 +0900

    blog - ud & comment

---
+++ board/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render,redirect, get_object_or_404
+from .models import *
+# views.py
+
+def home(req):
+    blog_objects = Blog.objects.all()
+    return render(req, 'home.html', {'data': blog_objects})
+
+def post_create(req):
+    if req.method == 'POST':
+        blog_object = Blog()
+        blog_object.title = req.POST['title']
+        blog_object.body = req.POST['body']
+        blog_object.save()
+        return redirect('/blog/'+str(blog_object.id))
+    return render(req, 'post_create.html')
+
+def post_read(req, id):
+    blog_object = get_object_or_404(Blog, pk=id)
+    comments = blog_object.comment_set.all()
+    c = blog_object.comment_set.first()
+    print(c)
+    print(comments)
+    context = {
+        'data' : blog_object,
+        'comments' : comments,
+    }
+    return render(req, 'post_read.html', context)
+
+def post_edit(req, id):
+    blog_object = get_object_or_404(Blog, pk = id)
+    return render(req,'post_edit.html',{'data' : blog_object})
+
+def post_update(req, id):
+    blog_object = get_object_or_404(Blog, pk = id)
+    blog_object.title = req.POST['title']
+    blog_object.body = req.POST['body']
+    blog_object.save()
+    return redirect('/blog/'+str(id))
+
+def comment_create(req,id):
+    if req.method == 'POST':
+        blog_object = get_object_or_404(Blog, pk = id)
+        blog_object.comment_set.create(body = req.POST['comment'])
+    return redirect('/blog/'+str(id))
